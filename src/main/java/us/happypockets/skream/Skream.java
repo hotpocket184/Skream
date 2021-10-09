24ac21a64a4d77c6d5458070158167454d9a0112
hapily04 <ryanbauwens1@gmail.com>
2021-10-08 23:50:14 -0500

    Initial commit

---
+++ src/main/java/us/happypockets/skream/Skream.java
@@ -0,0 +1,17 @@
+package us.happypockets.skream;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Skream extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
