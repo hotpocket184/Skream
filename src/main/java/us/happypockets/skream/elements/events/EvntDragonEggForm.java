255e9486d8b9baf3b5765c1f821d0705febdaf96
hotpocket184 <ml0938622@gmail.com>
2021-12-11 18:00:56 -0500

    Delay of Spawner

--- src/main/java/us/happypockets/skream/elements/events/EvntDragonEggForm.java
+++ src/main/java/us/happypockets/skream/elements/events/EvntDragonEggForm.java
@@ -1,2 +1,28 @@
-package us.happypockets.skream.elements.events;public class EvntDragonEggForm {
+package us.happypockets.skream.elements.events;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.SkriptEvent;
+import ch.njol.skript.lang.util.SimpleEvent;
+import ch.njol.skript.registrations.EventValues;
+import ch.njol.skript.util.Getter;
+import io.papermc.paper.event.block.DragonEggFormEvent;
+import org.bukkit.block.Block;
+import org.bukkit.boss.DragonBattle;
+
+public class EvntDragonEggForm extends SimpleEvent {
+    static {
+        Skript.registerEvent("Dragon Egg Form", SimpleEvent.class, DragonEggFormEvent.class, "dragon egg (form|make|create) [event]");
+        EventValues.registerEventValue(DragonEggFormEvent.class, Block.class, new Getter<Block, DragonEggFormEvent>() {
+            public Block get(DragonEggFormEvent e) {
+                return e.getBlock();
+            }
+
+        }, 0);
+        EventValues.registerEventValue(DragonEggFormEvent.class, DragonBattle.class, new Getter<DragonBattle, DragonEggFormEvent>() {
+            public DragonBattle get(DragonEggFormEvent e) {
+                return e.getDragonBattle();
+            }
+
+        }, 0);
+    }
 }
